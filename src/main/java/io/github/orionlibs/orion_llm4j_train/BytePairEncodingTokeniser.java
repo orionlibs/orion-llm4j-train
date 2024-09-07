8e8c4d494afe2582e9e721cd34d49e2c2fc3e86c
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-09-07 19:45:43 +0100

    implemented byte-pair encoding tokeniser and utilities

---
+++ src/main/java/io/github/orionlibs/orion_llm4j_train/BytePairEncodingTokeniser.java
@@ -0,0 +1,129 @@
+package io.github.orionlibs.orion_llm4j_train;
+
+import io.github.orionlibs.orion_assert.Assert;
+import io.github.orionlibs.orion_tuple.Pair;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Does not handle the regular expression splitting pattern.
+ * Does not handle any special tokens.
+ */
+public class BytePairEncodingTokeniser extends Tokeniser
+{
+    public BytePairEncodingTokeniser()
+    {
+        super();
+    }
+
+
+    @Override
+    public void train(String text, int vocabularySize)
+    {
+        Assert.isGreaterOrEqualTo(vocabularySize, 256, "vocabularySize has to be >= 256");
+        int numberOfMergesToDo = vocabularySize - 256;
+        //input text preprocessing
+        byte[] textBytes = text.getBytes(StandardCharsets.UTF_8);
+        //list of integers in range 0..255
+        List<Integer> tokenIDs = IntStream.range(0, textBytes.length)
+                        .mapToObj(i -> (int)textBytes[i])   // box each byte as Byte
+                        .collect(Collectors.toList());
+        //iteratively merge the most common pairs to create new tokens
+        Map<Pair<Integer, Integer>, Integer> mergesTemp = new HashMap<>();
+        Map<Integer, byte[]> vocabularyTemp = new HashMap<>();
+        for(int i = 0; i < 256; i++)
+        {
+            vocabularyTemp.put(i, new byte[] {(byte)i});
+        }
+        for(int i = 0; i < numberOfMergesToDo; i++)
+        {
+            //count up the number of times every consecutive pair appears
+            Map<Pair<Integer, Integer>, Integer> frequenciesOfConsecutivePairs = Utils.getFrequencyOfConsecutivePairs(tokenIDs);
+            //find the pair with the highest count
+            Pair<Integer, Integer> pairWithHighestFrequency = frequenciesOfConsecutivePairs.entrySet()
+                            .stream()
+                            .max(Entry.comparingByValue())
+                            .map(Entry::getKey)
+                            .orElse(null);
+            //mint a new token: assign it the next available id
+            int idx = 256 + i;
+            //replace all occurrences of pair in ids with idx
+            tokenIDs = Utils.merge(tokenIDs, pairWithHighestFrequency, idx);
+            //save the merge
+            mergesTemp.put(pairWithHighestFrequency, idx);
+            byte[] firstArray = vocabularyTemp.get(pairWithHighestFrequency.getFirst());
+            byte[] secondArray = vocabularyTemp.get(pairWithHighestFrequency.getSecond());
+            byte[] temp = new byte[firstArray.length + secondArray.length];
+            System.arraycopy(firstArray, 0, temp, 0, firstArray.length);
+            System.arraycopy(secondArray, 0, temp, firstArray.length, secondArray.length);
+            vocabularyTemp.put(idx, temp);
+        }
+        this.merges = mergesTemp;
+        this.vocabulary = vocabularyTemp;
+    }
+
+
+    @Override
+    public List<Integer> encode(String text)
+    {
+        byte[] textBytes = text.getBytes(StandardCharsets.UTF_8);
+        //list of integers in range 0..255
+        List<Integer> tokenIDs = IntStream.range(0, textBytes.length)
+                        .mapToObj(i -> (int)textBytes[i])   // box each byte as Byte
+                        .collect(Collectors.toList());
+        while(tokenIDs.size() >= 2)
+        {
+            Map<Pair<Integer, Integer>, Integer> frequenciesOfConsecutivePairs = Utils.getFrequencyOfConsecutivePairs(tokenIDs);
+            //find the pair with the lowest count
+            Pair<Integer, Integer> pairWithLowestFrequency = frequenciesOfConsecutivePairs.entrySet()
+                            .stream()
+                            .min(Entry.comparingByValue())
+                            .map(Entry::getKey)
+                            .orElse(null);
+            //subtle: if there are no more merges available, the key will
+            //result in an inf for every single pair, and the min will be
+            //just the first pair in the list, arbitrarily
+            //we can detect this terminating case by a membership check
+            if(!merges.containsKey(pairWithLowestFrequency))
+            {
+                break;//nothing else can be merged anymore
+            }
+            //otherwise let's merge the best pair (lowest merge index)
+            Integer idx = merges.get(pairWithLowestFrequency);
+            tokenIDs = Utils.merge(tokenIDs, pairWithLowestFrequency, idx);
+        }
+        return tokenIDs;
+    }
+
+
+    @Override
+    public String decode(List<Integer> tokenIDs)
+    {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        for(int id : tokenIDs)
+        {
+            byte[] vocabBytes = vocabulary.get(id);
+            if(vocabBytes != null)
+            {
+                try
+                {
+                    byteStream.write(vocabBytes);
+                }
+                catch(IOException e)
+                {
+                    // Handle IOException if it occurs
+                    e.printStackTrace();
+                }
+            }
+        }
+        // Convert the byte array to a string, using UTF-8 with replacement for invalid sequences
+        return new String(byteStream.toByteArray(), StandardCharsets.UTF_8);
+    }
+}
---
+++ src/test/java/io/github/orionlibs/orion_llm4j_train/UtilsTest.java
@@ -0,0 +1,50 @@
+package io.github.orionlibs.orion_llm4j_train;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.orionlibs.orion_tuple.Pair;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+//@Execution(ExecutionMode.CONCURRENT)
+public class UtilsTest extends ATest
+{
+    @Test
+    void test_getFrequencyOfConsecutivePairs()
+    {
+        //[1, 2, 3, 1, 2] -> {(1, 2): 2, (2, 3): 1, (3, 1): 1}
+        Map<Pair<Integer, Integer>, Integer> result = Utils.getFrequencyOfConsecutivePairs(Arrays.asList(1, 2, 3, 1, 2));
+        assertEquals(3, result.size());
+        assertEquals(2, result.get(Pair.of(1, 2)));
+        assertEquals(1, result.get(Pair.of(2, 3)));
+        assertEquals(1, result.get(Pair.of(3, 1)));
+    }
+
+
+    @Test
+    void test_getFrequencyOfConsecutivePairs2()
+    {
+        //[1, 2, 3, 1, 2] -> {(1, 2): 2, (2, 3): 1, (3, 1): 1}
+        Map<Pair<Integer, Integer>, Integer> frequencies = new HashMap<>();
+        frequencies.put(Pair.of(1, 2), 1);
+        Map<Pair<Integer, Integer>, Integer> result = Utils.getFrequencyOfConsecutivePairs(Arrays.asList(1, 2, 3, 1, 2));
+        assertEquals(3, result.size());
+        assertEquals(2, result.get(Pair.of(1, 2)));
+        assertEquals(1, result.get(Pair.of(2, 3)));
+        assertEquals(1, result.get(Pair.of(3, 1)));
+    }
+
+
+    @Test
+    void test_merge()
+    {
+        List<Integer> result = Utils.merge(Arrays.asList(1, 2, 3, 1, 2), Pair.of(1, 2), 4);
+        assertEquals(Arrays.asList(4, 3, 4), result);
+    }
+}
